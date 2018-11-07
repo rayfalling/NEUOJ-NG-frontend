a3efd4f97a011048508a0d03b6547eba7872cd0f
LI JIAHAO <jh.li@maimemo.com>
2018-11-07 17:50:25 +0800

    :beers: sign in sign up

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,13 +8,45 @@
           </v-toolbar>
           <v-card-text>
             <v-form>
-              <v-text-field prepend-icon="person" name="username" label="Username" type="text"></v-text-field>
-              <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+              <v-text-field @keyup="onTextChange" @change="onTextChange"
+                            v-model="username"
+                            prepend-icon="person" name="username" label="Username" type="text">
+              </v-text-field>
+              <v-text-field v-model="password"
+                            @keypress="onPasswordChange" @keypress.enter="submit"
+                            id="password" prepend-icon="lock" name="password" label="Password" type="password">
+              </v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
+            <p class="font-weight-bold" v-if="markCreate">
+              It appears this username has not been taken. <br/>
+              Shall we register a new membership with the given credentials?
+            </p>
+            <p class="font-weight-bold error--text" v-else-if="markReset">
+              You provided the wrong password. <br/>
+              If you believe this should be right, we can reset it for you.
+            </p>
+            <p class="font-weight-bold" v-else-if="markLogin"
+              v-html="
+                justReset
+                ? `An email with a reset link has been sent to you. <br/>
+                  Once you opened that link, you'll be ready to login.`
+                : `It appears this username has already been taken. <br/>
+                  If you believe you're the owner, please provide credentials to sign in.`
+              "
+            >
+            </p>
             <v-spacer></v-spacer>
-            <v-btn color="primary" to="/join">Continue</v-btn>
+            <v-btn @click="reset" color="error" v-if="markReset">
+              Reset
+            </v-btn>
+            <v-btn @click="createAccount" color="primary" v-else-if="markCreate">
+              Register
+            </v-btn>
+            <v-btn @click="login" v-else-if="markLogin">
+              Login
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -27,9 +59,53 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Login extends Vue {
+  private markCreate = false;
+  private markLogin = false;
+  private markReset = false;
+  private justReset = false;
+
+  private username = '';
+  private password = '';
+
+  private onTextChange() {
+    console.log(this.username);
+    if (Math.floor(Math.random() * 100 % 2)) {
+      this.markLogin = true;
+      this.markCreate = false;
+    } else {
+      this.markLogin = false;
+      this.markCreate = true;
+    }
+    this.markReset = false;
+    this.justReset = false;
+  }
+
+  private onPasswordChange() {
+    this.markReset = false;
+    this.justReset = false;
+  }
+
+  private createAccount() {
+    this.$router.push('/join');
+  }
+
+  private login() {
+    this.markReset = true;
+  }
+
+  private reset() {
+    this.justReset = true;
+    this.markReset = false;
+    this.markLogin = true;
+  }
+
+  private submit() {
+    if (this.markLogin) return this.login();
+    else if (this.markCreate) return this.createAccount();
+  }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
 
 </style>
