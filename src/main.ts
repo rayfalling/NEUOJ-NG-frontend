fa27c579888c125e4d762bb92deec6d639dee932
LI JIAHAO <jh.li@maimemo.com>
2018-11-07 14:37:02 +0800

    :art: configure tslint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import Vue from 'vue';
+
 import App from './App.vue';
 import './plugins/vuetify';
 import router from './router';
